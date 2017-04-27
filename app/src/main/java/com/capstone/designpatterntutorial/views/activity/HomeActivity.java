2d33ce2178ca0e10bce462c10b0a7f1cb9d04506
Venugopal Rao Gubbala <venugopal.raog@gmail.com>
2017-04-27 07:20:59 -0400

    Added Dependency Injection

--- app/src/main/java/com/capstone/designpatterntutorial/views/activity/HomeActivity.java
+++ app/src/main/java/com/capstone/designpatterntutorial/views/activity/HomeActivity.java
@@ -1,31 +1,40 @@
 package com.capstone.designpatterntutorial.views.activity;
 
+import android.content.ContentResolver;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.capstone.designpatterntutorial.R;
 import com.capstone.designpatterntutorial.database.DesignPatternContract;
+import com.capstone.designpatterntutorial.di.MyApplication;
+
+import javax.inject.Inject;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private String TAG = HomeActivity.class.getSimpleName();
 
+    @Inject
+    ContentResolver mContentResolver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ((MyApplication) getApplication()).getAppComponent().inject(this);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -89,7 +98,7 @@ public class HomeActivity extends AppCompatActivity
 
         if (id == R.id.design_pattern) {
             // Handle the camera action
-            Cursor cursor =  getContentResolver().query(DesignPatternContract.CategoryListEntry.CONTENT_URI,
+            Cursor cursor =  mContentResolver.query(DesignPatternContract.CategoryListEntry.CONTENT_URI,
                     null,
                     null,
                     null,
